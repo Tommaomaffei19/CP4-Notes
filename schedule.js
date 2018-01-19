140f0f0db14f7a6624233c6d3196ee05b7de9943
Tommaomaffei19 <Tommaso.maffei19@alumni.loyola.ca>
2018-01-19 10:02:03 -0500

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,15 @@
+/*
+function searchTable() {
+    var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("myInput") ;
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+  
+*/
+
+/* MR Degano wants you to just try and save something from a text box to a variable in javascript :) 
+- do you know how to modify html content with js?
+-do you know how to save html content as a variable?
+-do you know how to write a javascript function?
+	-can you write a function to work after pressing a button?
+*/
