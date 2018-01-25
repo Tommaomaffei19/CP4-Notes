84d9fb90de73426305ad7d3e6c5ec627b4f8e4e9
Tommaomaffei19 <Tommaso.maffei19@alumni.loyola.ca>
2018-01-25 10:22:06 -0500

    a few minor changes

--- schedule.js
+++ schedule.js
@@ -1,15 +1,17 @@
-/*
-function searchTable() {
-    var input, filter, found, table, tr, td, i, j;
-    input = document.getElementById("myInput") ;
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-  
-*/
-
-/* MR Degano wants you to just try and save something from a text box to a variable in javascript :) 
-- do you know how to modify html content with js?
--do you know how to save html content as a variable?
--do you know how to write a javascript function?
-	-can you write a function to work after pressing a button?
-*/
+switch 
+ 
+ case 'math'
+ case 'english'
+ case 'gym'
+ case 'Advisors'
+ case 'religion' 
+ case 'speech'
+ case 'Ntg'
+ case 'history'
+ case 'theater-arts'
+ case 'computer-programing'
+ case 'Hr'
+ case 'french'
+ case 'english'
+ case 'science'
+ 
