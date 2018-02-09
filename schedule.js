ed3ef67bdcfcaec7b7c7694597e213217bc94a38
Tommaomaffei19 <Tommaso.maffei19@alumni.loyola.ca>
2018-02-09 08:49:59 -0500

    minor changes to js

--- schedule.js
+++ schedule.js
@@ -1,17 +1,46 @@
-switch 
+switch expr = 'math';
+{
  
- case 'math'
- case 'english'
- case 'gym'
- case 'Advisors'
- case 'religion' 
- case 'speech'
- case 'Ntg'
- case 'history'
- case 'theater-arts'
- case 'computer-programing'
- case 'Hr'
- case 'french'
- case 'english'
- case 'science'
+ case 'math':
+break;
+ console.log ('i have math')	
+ case 'english':
+ break;
+ 
+ case 'gym':
+ break;
+ 
+ case 'Advisors':
+ break;
+ 
+ case 'religion':
+ break;
+ 
+ case 'speech':
+ break;
+ 
+ case 'Ntg':
+ break;
+ 
+ case 'history':
+ break;
+ 
+ case 'theater-arts':
+ break;
+ 
+ case 'computer-programing':
+ break;
+ 
+ case 'Hr':
+ break;
+ 
+ case 'french':
+ break;
+ 
+ case 'english':
+ break;
+ 
+ case 'science':
+ break;
+}
  
