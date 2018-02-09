abb631d80bc796898717bf4ffdc9b560558a8b09
tommaso <tommaso.maffei19@alumni.loyola.ca>
2018-02-09 17:12:24 -0500

    stayed after school
    fixed up js and started getting closer to getting a working search bar

--- schedule.js
+++ schedule.js
@@ -1,46 +1,34 @@
-switch expr = 'math';
-{
- 
- case 'math':
-break;
- console.log ('i have math')	
- case 'english':
- break;
- 
- case 'gym':
- break;
- 
- case 'Advisors':
- break;
- 
- case 'religion':
- break;
- 
- case 'speech':
- break;
- 
- case 'Ntg':
- break;
- 
- case 'history':
- break;
- 
- case 'theater-arts':
- break;
- 
- case 'computer-programing':
- break;
- 
- case 'Hr':
- break;
- 
- case 'french':
- break;
- 
- case 'english':
- break;
- 
- case 'science':
- break;
+
+
+var expr = documen.getElementById("myImput");
+switch (expr){
+  case 'math'
+    console.log ('i have math');
+    break;
+  case 'english':
+    break;
+  case 'gym':
+    break;
+  case 'Advisors':
+    break;
+  case 'religion':
+    break;
+  case 'speech':
+    break;
+  case 'Ntg':
+    break;
+  case 'history':
+    break;
+  case 'theater-arts':
+    break;
+  case 'computer-programing':
+    break;
+  case 'Hr':
+    break;
+  case 'french':
+    break;
+  case 'english':
+    break;
+  case 'science':
+    break;
 }
- 
